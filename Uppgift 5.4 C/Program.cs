b93539ad90abd67e7e0a42a4a9e947d6d8c47516
Br0deNN <theo.broden@gmail.com>
2023-11-28 20:22:56 +0100

    Add project files.

---
+++ Uppgift 5.4 C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_5._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "Olle", "Theo", "Alfred", "Emil", "Markus" };
+
+            Array.Sort(namn);
+
+           foreach (string x in namn)
+            {
+                Console.WriteLine(x);
+            }
+
+            
+        }
+    }
+}
